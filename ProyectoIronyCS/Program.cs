fa4ebc3f44a640d412994687fa8f386193501bee
201504481 <eljulio.arango97@gmail.com>
2019-07-23 05:18:42 -0600

    ProyectoIrony

---
+++ ProyectoIronyCS/Program.cs
@@ -0,0 +1,21 @@
+﻿using ProyectoIronyCS.sol.com.analizador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoIronyCS
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText("entrada.txt");
+            Sintactico sintac = new Sintactico();
+            sintac.analizar(text);
+        }
+    }
+}
---
+++ ProyectoIronyCS/analizador/Gramatica.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace ProyectoIronyCS.sol.com.analizador
+{
+    class Gramatica : Grammar
+    {
+        public Gramatica() : base(caseSensitive: false) {
+
+            #region ER
+            var ENTERO = new NumberLiteral("entero");
+            var DECIMAL = new RegexBasedTerminal("Decimal", "[0-9]+'.'[0-9]+");
+            #endregion
+
+            #region Terminales
+            var REVALUAR = ToTerm("Evaluar");
+            var PTCOMA = ToTerm(";");
+            var PARIZQ = ToTerm("(");
+            var PARDER = ToTerm(")");
+            var CORIZQ = ToTerm("[");
+            var CORDER = ToTerm("]");
+            var MAS = ToTerm("+");
+            var MENOS = ToTerm("-");
+            var POR = ToTerm("*");
+            var DIVIDIDO = ToTerm("/");
+            var mod = ToTerm("%");
+
+            RegisterOperators(1, MAS, MENOS);
+            RegisterOperators(2, POR, DIVIDIDO);
+
+            #endregion
+
+            #region No Terminales
+            NonTerminal ini = new NonTerminal("ini");
+            NonTerminal instruccion = new NonTerminal("instruccion");
+            NonTerminal instrucciones = new NonTerminal("instrucciones");
+            NonTerminal expresion = new NonTerminal("expresion");
+            #endregion
+
+            #region Gramatica
+            ini.Rule = instrucciones;
+
+            instrucciones.Rule = instruccion + instrucciones
+                | instruccion;
+
+            instruccion.Rule = REVALUAR + CORIZQ + expresion + CORDER + PTCOMA;
+
+            expresion.Rule = MENOS + expresion
+                | expresion + MAS + expresion
+                | expresion + MENOS + expresion
+                | expresion + POR + expresion
+                | expresion + DIVIDIDO + expresion
+                | ENTERO
+                | DECIMAL
+                | PARIZQ + expresion + PARDER;
+
+            #endregion
+
+            #region Preferencias
+            this.Root = ini;
+            #endregion
+        }
+    }
+}
