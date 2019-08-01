c8861dd8d25180f9924939a938fa883d3112b8f6
ericknavarro <ecrnd25@gmail.com>
2019-08-01 12:18:59 -0600

    Some format improvements

--- ProyectoIronyCS/analizador/Sintactico.cs
+++ ProyectoIronyCS/analizador/Sintactico.cs
@@ -11,38 +11,45 @@ namespace ProyectoIronyCS.sol.com.analizador
     class Sintactico
     {
 
-        public void analizar(String cadena) {
+        public void analizar(String cadena)
+        {
             Gramatica gramatica = new Gramatica();
             LanguageData lenguaje = new LanguageData(gramatica);
             Parser parser = new Parser(lenguaje);
             ParseTree arbol = parser.Parse(cadena);
             ParseTreeNode raiz = arbol.Root;
-            
+
             instrucciones(raiz.ChildNodes.ElementAt(0));
-            
+
         }
 
-        public void instrucciones(ParseTreeNode actual) {
+        public void instrucciones(ParseTreeNode actual)
+        {
             if (actual.ChildNodes.Count == 2)
             {
                 instruccion(actual.ChildNodes.ElementAt(0));
                 instrucciones(actual.ChildNodes.ElementAt(1));
             }
-            else {
+            else
+            {
                 instruccion(actual.ChildNodes.ElementAt(0));
             }
         }
 
-        public void instruccion(ParseTreeNode actual) {
-            Console.WriteLine("El valor de la expresion es: " + expresion(actual.ChildNodes.ElementAt(2)));
+        public void instruccion(ParseTreeNode actual)
+        {
+            System.Diagnostics.Debug.WriteLine("El valor de la expresion es: " + expresion(actual.ChildNodes.ElementAt(2)));
         }
 
-        public double expresion(ParseTreeNode actual) {
-            if (actual.ChildNodes.Count == 3) {
+        public double expresion(ParseTreeNode actual)
+        {
+            if (actual.ChildNodes.Count == 3)
+            {
                 string tokenOperador = actual.ChildNodes.ElementAt(1).ToString().Split(' ')[0];
-                switch (tokenOperador) {
+                switch (tokenOperador)
+                {
                     case "+":
-                        return expresion(actual.ChildNodes.ElementAt(0))  + expresion(actual.ChildNodes.ElementAt(2));
+                        return expresion(actual.ChildNodes.ElementAt(0)) + expresion(actual.ChildNodes.ElementAt(2));
                     case "-":
                         return expresion(actual.ChildNodes.ElementAt(0)) - expresion(actual.ChildNodes.ElementAt(2));
                     case "*":
